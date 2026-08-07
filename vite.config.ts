8b8b7a532870145bf31a68ca6efa9351f666ddf9
ss360marketing-creator <Ss360marketing@gmail.com>
2026-08-07 16:20:18 +0500

    Update application title to Salman Marketplace

--- vite.config.ts
+++ vite.config.ts
@@ -93,7 +93,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? 'Figma Make App'
+  const title = config.title ?? 'Salman Marketplace'
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
